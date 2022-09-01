a84337ac1ce05318050293ef9e2a4f71e4aec4fc
cs1812157 <98776469+cs1812157@users.noreply.github.com>
2022-09-01 12:42:07 +0500

    Add files via upload

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -94,6 +94,11 @@ const Register = () => {
                 formValues.password
             )
         );
+        setFormValues({
+            ...formValues,
+            password: "",
+            confirmPassword: ""
+        });
     };
 
     useEffect(() => {
